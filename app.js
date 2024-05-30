220e83e24aaa7edf81304d9b26614c9852fa782d
Dispatch2 <Dispatch2@Saundersph.ca>
2024-05-30 15:13:10 -0400

    sotre

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-bool = 1 == "1"
+let cash = 109
+let price = 40
 
-left at 
+if (cash > price) {
+    console.log(`here is ${cash - price} dollars change`)
+}
+
+else if (cash === price) {
+    console.log9`you paid the exact amount thank you`
+}
+
+else {
+    console.log(`thats not enough money u poor bitch, you still owe me ${(cash - price) * -1} dollars`)
+}
