753bc6f77b6ff2a9c2aa73c6c10fdd91a560ad81
Dispatch2 <Dispatch2@Saundersph.ca>
2024-07-03 14:38:55 -0400

    arrays

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-let arr = [20, 30, 40, 50, 100]
+let arr = ["A", "A+", "FAIL","B-", "C+", "FAIL",]
 
-let newArr = arr.filter ((element) => {
-    console.log(element)
-    return true
-})
+let newArr = arr.filter (element => element !== "FAIL" )
+
+console.log(newArr)
