ad50d77a49049412ac05ab8b17cb6e22831e9ccf
Nixon <nixonjayatilake@gmail.com>
2024-08-21 18:29:42 -0400

    objects

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
+let users = [{
+  username: "nixon",
+  email: "nixon@gmail.com",
+  password: "password1",
+  subStatus: "VIP",
+  discordId: "Mike",
+  lessonsCompleted: "13",}]
+
 let user = {
   username: "nixon",
   email: "nixon@gmail.com",
+  password: "password1",
   subStatus: "VIP",
+  discordId: "Mike",
+  lessonsCompleted: "13",
 };
 
-console.log(user.username);
+function register(
+  name,
+  email,
+  password,
+  subStatus,
+  discordId,
+  lessonsCompleted
+) {
+  let user = {
+    username: name,
+    email: email,
+    password: password,
+    subStatus: subStatus,
+    discordId: discordId,
+    lessonsCompleted: lessonsCompleted,
+  };
+ users.push(user)
+}
+
+register("zack", "bobvance@vancefiridgeorations.com", "password", "vip", "13");
+
+console.log(users)
