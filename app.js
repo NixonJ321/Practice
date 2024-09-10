08212f137716825e4c896077487d1dfc3e0f8e65
Nixon <nixonjayatilake@gmail.com>
2024-09-10 18:49:17 -0400

    doing better

--- app.js
+++ app.js
@@ -1,32 +1,26 @@
-function progressiveSum(num) {
-    let sum = 0
-    for (let i = 0; i < num+1; ++i) {
-        sum = sum + i
-    }
-    return sum
-}
-
-console.log(progressiveSum(300));
-
-function arrSum(arr) {
-    let base = 0
+function getMax(arr) {
+    let max = arr[0]
     for (let i = 0; i < arr.length; ++i) {
-        base = base + arr[i]
+       if(max < arr[i]) {
+        max = arr[i]
+       }
     }
-    return console.log(base)
+    return max
 }
 
-console.log(arrSum([2,2,2]))
+console.log(getMax([-100,-20000,-100]))
 
-function arrLength(arr) {
-    return arr[arr.length-1]
+function calcTime(time) {
+    let min = Math.floor(time / 60)
+    let sec = time % 60  
+    if ( min < 10) {
+        min = '0' + min
+    }
+    if ( sec < 10) {
+        sec = '0' + sec
+    }
+    return min + ':' + sec
 }
 
-console.log(arrLength([1,2,3,3,15]))
-
-function filterOutFalsy(input1,input2) {
-    return input1 == false ? input1 : input2
-  }
-  
-  console.log(filterOutFalsy(true,500))
+console.log(calcTime(549))
 
