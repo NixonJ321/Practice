d9157581c5e30d67a5895ae01e72bd312c2e4cfb
Nixon <nixonjayatilake@gmail.com>
2024-09-24 18:25:58 -0400

    much better now with first half of questions

--- app.js
+++ app.js
@@ -1,26 +1,58 @@
-function getMax(arr) {
-    let max = arr[0]
-    for (let i = 0; i < arr.length; ++i) {
-       if(max < arr[i]) {
-        max = arr[i]
-       }
+function largestNum(arr) {
+    let num = arr[0]
+    for ( i = 0; i < arr.length; ++i) {
+        if (num < arr[i]) {
+            num = arr[i]
+        }
     }
-    return max
+    return num
 }
 
-console.log(getMax([-100,-20000,-100]))
+console.log(largestNum([-100,-2,-3,4]))
 
-function calcTime(time) {
-    let min = Math.floor(time / 60)
-    let sec = time % 60  
-    if ( min < 10) {
-        min = '0' + min
+function calcTime(secs) {
+    let calcMins = Math.floor(secs / 60)
+    let calcSecs = secs % 60
+    if (calcMins < 10) {
+        calcMins = '0' + calcMins
     }
-    if ( sec < 10) {
-        sec = '0' + sec
+    if (calcSecs < 10) {
+        calcSecs = '0' + calcSecs
     }
-    return min + ':' + sec
+    return calcMins + ':' + calcSecs
 }
 
-console.log(calcTime(549))
+console.log(calcTime(69))
 
+function progressSum(num) {
+    let sum = 0
+    for (i = 0; i <= num; ++i) {
+        sum = sum + i
+    }
+    return sum
+}
+
+console.log(progressSum(3))
+
+function arrsum(arr) {
+    let sum = 0
+    for (i = 0; i < arr.length ; ++i) {
+        sum = sum + arr[i]
+      
+    }
+    return sum
+}
+
+console.log(arrsum([1,1,1,90]))
+
+function arrlength(arr) {
+    return arr.length
+}
+
+console.log(arrlength([1]))
+
+function filterfalse(input1, input2) {
+    return input1 == false ? input1 : input2
+}
+
+console.log(filterfalse(true,500))
