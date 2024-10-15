1842abea562bf754a2dcb836b19f91e75e84e731
Nixon <nixonjayatilake@gmail.com>
2024-10-15 18:53:25 -0400

    medium finished

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-function reverseString(str) {
-    let reverseStr = ''
-    for (i = 0; i < str.length; ++i)
-        reverseStr = str[i] + reverseStr
-    return reverseStr
-}
-
-console.log(reverseString("Nixon"))
-
-function Zeros(arr) {
-    for ( i = 0; i < arr.length; ++i) 
-        arr[i] = 0
-    return arr
-
-   
-}
-
-console.log(Zeros([1,2,4,5,6]))
+function convertToBool(arr) {
+    return arr.map(elem => !!elem)
+    }
+    
+    console.log(convertToBool([1,1000,0,undefined,null,[],'',"boobs", NaN]))
+    
+    function filterFalsey(arr) {
+        let truthyArr = []
+        for (let i = 0; i < arr.length; ++i) {
+            if (arr[i] != false) {
+                truthyArr.push(arr[i])
+            }
+        }
+        return truthyArr
+    }
+    
+    console.log(filterFalsey([0,1,"0",'null','','boobs', 3]))
+    
+    function noApples(arr) {
+        return arr.filter(elem => elem !== 'apple')
+    }
+    
+    
+    console.log(noApples(['banana', 'apple', 'orange','apple', 'kiwi']))
+    
+    function fuckApples(arr) {
+        let noApples = []
+     for (let i = 0; i < arr.length; ++i) {
+        if(arr[i] !== 'apples'){
+            noApples.push(arr[i])
+        }
+     }
+     return noApples
+    }
+    
+    console.log(fuckApples(['apples','banana','money','apples','orange']))
