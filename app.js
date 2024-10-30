c7180cb8c73074df754771542287e5c569c18672
Nixon <nixonjayatilake@gmail.com>
2024-10-30 18:54:21 -0400

    really getting it

--- app.js
+++ app.js
@@ -1,36 +1,74 @@
-function convertToBool(arr) {
-    return arr.map(elem => !!elem)
+function getMax (arr) {
+let max = arr[0]
+for (let i = 0; i < arr.length; ++i) {
+    if (max < arr[i]) {
+        max = arr[i]
+    }
+}
+return max
+}
+
+console.log(getMax([-300,-100,200]))
+
+function calcTime(num) {
+    let min = Math.floor(num / 60)
+    let sec = num % 60
+    if (min < 10) {
+        min = '0' + min
+    }
+    if (sec < 10) {
+        sec = '0' + sec
+    }
+
+    return min + ":" + sec
+}
+
+console.log(calcTime(50))
+
+function addtionButHarder(num) {
+    let sum = 0
+    for (let i = 0; i < num + 1; ++i) {
+        sum = sum + i
+    }
+    return sum
+}
+
+console.log(addtionButHarder(494908))
+
+function addtion(arr) {
+    let sum = 0
+    for (let i = 0; i < arr.length; ++i) {
+        sum = sum + arr[i]
+    }
+    return sum
+}
+
+console.log(addtion([100,200,500]))
+
+function tryIng(arr) {
+    return arr[arr.length - 1]
     }
     
-    console.log(convertToBool([1,1000,0,undefined,null,[],'',"boobs", NaN]))
+    console.log(tryIng([2,3,1,'poo']))
     
-    function filterFalsey(arr) {
-        let truthyArr = []
+    function lastElem(arr) {
+        let sum = -1
         for (let i = 0; i < arr.length; ++i) {
-            if (arr[i] != false) {
-                truthyArr.push(arr[i])
-            }
+            sum = sum + 1
         }
-        return truthyArr
+        return arr[sum]
     }
     
-    console.log(filterFalsey([0,1,"0",'null','','boobs', 3]))
+    console.log(lastElem([1,,'boo',1,1000]))
     
-    function noApples(arr) {
-        return arr.filter(elem => elem !== 'apple')
+    function fuckYou(arr) {
+        return arr.length
     }
     
+    console.log(fuckYou([1,2,3,4]))
     
-    console.log(noApples(['banana', 'apple', 'orange','apple', 'kiwi']))
-    
-    function fuckApples(arr) {
-        let noApples = []
-     for (let i = 0; i < arr.length; ++i) {
-        if(arr[i] !== 'apples'){
-            noApples.push(arr[i])
-        }
-     }
-     return noApples
+    function filterfalsey(elem1,elem2) {
+        return elem1 = !!elem1 == false ? elem1 : elem2
     }
     
-    console.log(fuckApples(['apples','banana','money','apples','orange']))
+    console.log(filterfalsey(10,500))
