3ac46a13d2b605ba8a0bee6e84350b761d1f64fb
Nixon <nixonjayatilake@gmail.com>
2024-11-05 18:59:38 -0500

    holy these are harder than I rememebered

--- app.js
+++ app.js
@@ -1,74 +1,49 @@
-function reserveString(str) {
-    return str.split('').reverse().join('')
-}
+function sortObjects(arr) {
+    let sortedObjects = arr.sort((a, b) => a.price-b.price)
+    return sortedObjects
+    } 
+    
+    console.log(sortObjects
+        ([
+            {id:1, price: 50},
+            {id:2, price: 0},
+            {id:3, price: 500},
+        ])
+    )
 
-console.log(reserveString('Bingo was his Name Oh!'))
-function reserveString(str) {
-    let backwardsString = ''
-    for (let i = 0; i < str.length; ++i) {
-        backwardsString = str[i] + backwardsString
-    }
-    return backwardsString
-}
-
-console.log(reserveString('Bingo was his Name Oh!'))
-
-function boolValve(arr) {
-    return arr.map(elem => !!elem)
+    function sortObjects(arr) {
+        let sortedObjects = arr.sort(({price:a}, {price: b}) => a-b)
+        return sortedObjects
+        } 
+        
+        console.log(sortObjects
+            ([
+                {id:1, price: 50},
+                {id:2, price: 0},
+                {id:3, price: 500},
+            ])
+        )
+    
+    function sortHtoL(numbers) {
+        numbers.sort((a, b) => a - b)
+        return numbers
     }
     
-    console.log(boolValve([1,0,null,'sad',[],false,undefined,'']))
+    console.log(sortHtoL([3,-10000,756,2149312,2,100]))
     
-    function filterFalsey(arr) {
-        return arr.filter(elem => !!elem == true)
-     
-     }
-     
-     console.log(filterFalsey([1,0,[],NaN,null,undefined,'happy',true]))
-     
-     function filterApples(arr) {
-         return arr.filter(elem => elem !== 'apples')
-     
-     }
-     
-     console.log(filterApples(['apples','john','apples','oranges']))
-     
-     function filterApples(arr) {
-         let filteredArr = []
-         for ( let i = 0; i < arr.length; ++i) {
-             if (arr[i] !== 'apples') {
-                 filteredArr.push(arr[i])
-             }
-             
-         }
-         return filteredArr
-     }
-     
-     console.log(filterApples(['apples','coke',"apples","steak"]))
-     
-     
-     function reverseString(str) {
-         return str.split('').reverse().join('')
-     }
-     
-     console.log(reverseString('Hypothesis'))
-     
-     function Zeros(arr) {
-         for (let i = 0; i < arr.length; ++i) {
-           arr[i] = 0
-         }
-         return arr
-     }
-     
-     console.log(Zeros([1,2,3]))
-     
-     function reversed(str) {
-         let reversedString = ''
-         for ( let i = 0; i < str.length; ++i) {
-             reversedString = str[i] + reversedString
-         }
-         return reversedString
-     }
-     
-     console.log(reversed('Nixon'))
-
+    function ratingSys(rating) {
+        let ratings = ''
+        for (let i = 0; i < Math.floor(rating); ++i) {
+           ratings = ratings + '*'
+           if (i !== Math.floor(rating) - 1) {
+            ratings = ratings + ' '
+           }
+        }
+           if (!Number.isInteger(rating)) {
+            ratings = ratings + ' .'
+           }
+        return ratings
+    }
+    
+    console.log(ratingSys(4.5))
+    
