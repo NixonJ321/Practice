e999adaee7a9b725b56669ab7ec5421d69ea2b3c
Nixon <nixonjayatilake@gmail.com>
2024-11-20 18:56:27 -0500

    learning promises

--- app.js
+++ app.js
@@ -1,57 +1,21 @@
-function sortObjects (arr) {
+let emailRef = document.querySelector(".email");
+console.log(emailRef);
 
-    return arr.sort((a, b) => a.price - b.price)
-}
+fetch("https://jsonplaceholder.typicode.com/users/1")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
 
-console.log(sortObjects([
-    {id:1, price:50},
-    {id:2, price:30},
-    {id:3, price:60},
-    {id:4, price:10},
-    {id:5, price:20},
-]))
+  const emailRef = document.querySelector('.email')
 
-function sortHtoL(arr) {
-    return arr.sort((a, b) => a - b)
-    
-}
+async function main() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+    const data = await response.json()
+    console.log(data)
+    emailRef.innerHTML = data.email
+} 
 
-console.log(sortHtoL([1,40,-1,-432,109,42,23,100]))
-
-function showRating(num) {
-    let ratings = ''
-    for (let i = 0; i < Math.floor(num); ++i) {
-        if(i !== (Math.floor(num) - 1)) {
-            ratings = ratings + '* '
-        }
-        else {
-            ratings = ratings + '*'
-        }
-    }
-    if(!Number.isInteger(num)) {
-        ratings = ratings + '.'
-    }
-
-    return ratings
-}
-    
-    
-    console.log(showRating(4))
-
-    function showRating(num) {
-        let rating = ''
-        for (let i = 0; i < Math.floor(num); ++i) {
-            if(i !== Math.floor(num) - 1) {
-                rating = rating + '* '
-            }
-            else {
-                rating = rating + '*'
-            }
-        }
-        if(!Number.isInteger(num)) {
-            rating = rating + '.'
-        }
-        return rating
-    }
-    
-    console.log(showRating(4.5))
+main()
