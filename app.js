425736c8b162b787bc034c72ae83effb2388d236
Nixon <nixonjayatilake@gmail.com>
2024-12-03 18:19:35 -0500

    built dif

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
-async function postsByUser(uId) {
-  const promise = await fetch("https://jsonplaceholder.typicode.com/POSTS")
-
-  const result = await promise.json()
- 
-  const posts = result.filter(elem => elem.userId === uId)
-
-  console.log(posts)
+function sortHighToLow(arr) {
+  let sortedObj = arr.sort((a, b) => (b.price - a.price))
+  return sortedObj
 }
 
-console.log(postsByUser(6))
 
-async function completedPosts(completed) {
-  const promise = await fetch('https://jsonplaceholder.typicode.com/todos')
+console.log(sortHighToLow([
+  {id: 1, price: 50},
+  {id: 2, price: 500},
+  {id: 3, price: 10},
+  {id: 4, price: 30},
+  {id: 5, price: 65},
+  {id: 6, price: 99},
+]))
 
-  const result = await promise.json()
 
-  const sortedPosts = result.filter(elem => elem.completed === completed).slice(0,23)
-
-  console.log(sortedPosts)
+function sortlowToHigh(arr) {
+  let sortedarr = arr.sort((a, b) => (a - b))
+  return sortedarr
 }
 
-console.log(completedPosts(false))
+console.log(sortlowToHigh([1,-203,-2,100,99,23,-34,374]))
 
+function showRating(num) {
+  let rating = ''
+  for (let i = 0; i < Math.floor(num); ++i) {
+
+    if (i !== (Math.floor(num) - 1)) {
+      rating = rating + '* '
+    }
+
+    else (rating = rating + '*')
+  }
+  if(!Number.isInteger(num)) {
+    rating = rating + '.'
+  }
+  return rating
+}
+
+console.log(showRating(4.5))
